3d77dd315c363cfbe0bfc754457408980a484c4f
aang Muhammad <engoncode@gmail.com>
2025-12-01 23:59:16 +0700

    update

--- src/pages/AuthPages/Login.tsx
+++ src/pages/AuthPages/Login.tsx
@@ -58,7 +58,7 @@ const Login: React.FC = () => {
           <div className="p-8">
             {/* Logo */}
             <div className="flex justify-center mb-8">
-              <img src="images/logo/logo-dark.svg" alt="Logo" className="h-12 w-auto dark:hidden" />
+              <img src="images/logo/sd-logo-dark.svg" alt="Logo" className="h-12 w-auto dark:hidden" />
               <img src="images/logo/logo.svg" alt="Logo" className="h-12 w-auto hidden dark:block" />
             </div>
 
